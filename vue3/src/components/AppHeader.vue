fb1a29d8634e391958e1e176d055c8dbb0c2be08
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2022-10-19 17:49:22 +0600

    Dc Heros

---
+++ vue3/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="bg-gray-50 dark:bg-gray-700 mb-10">
+        <div class="py-3 px-4 mx-auto max-w-screen-xl md:px-6">
+            <div class="flex items-center">
+                <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Dc Heros</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue3/src/components/DcHeros.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="m-auto">
+        <h1 class="text-2xl text-center">DC Heros - {{ herosCount }}</h1>
+
+        <ul>
+            <li class="flex justify-between" v-for="(hero, index) in dcHeros" :key="hero.name">
+                <div>
+                    {{ hero.name }}
+                </div>
+                <button @click="remove(index)">x</button>
+            </li>
+        </ul>
+
+        <form class="mt-10" @submit.prevent="addHero">
+            <input class="border rounded py-2 px-4" v-model="newHero" placeholder="Type hero name here">
+            <button class="bg-blue-500 ml-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Add
+                Hero</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        herosCount() {
+            return this.dcHeros.length;
+        }
+    },
+    methods: {
+        addHero() {
+            if (this.newHero !== '') {
+                this.dcHeros.unshift({ name: this.newHero });
+                this.newHero = "";
+            } else {
+                alert("Hero name required");
+            }
+        },
+        remove(index) {
+            this.dcHeros = this.dcHeros.filter((hero, i) => i != index);
+        }
+    },
+    data() {
+        return {
+            newHero: "",
+            dcHeros: [
+                { name: "SuperGirl" },
+                { name: "Flash" },
+                { name: "Arrow" },
+                { name: "Batman" },
+                { name: "Superman" }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
